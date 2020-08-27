a8d6b33210c1966969da5e0347a45fde26f57b50
skar5268 <skar5268@gmail.com>
2020-08-28 03:00:04 +0800

    Initial commit

---
+++ js/all-dist.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".navbar-toggler").click((function(){$(".header").toggleClass("container"),$(".menu").toggleClass("container")}))}));
---
+++ js/all.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.navbar-toggler').click(function () {
+        $('.header').toggleClass("container");
+        $('.menu').toggleClass("container");
+    });
+});
